61fe4c0426b38384e27a01b811c68b24cf5c4719
rachen <rachen@central.tech>
2019-02-19 19:48:30 +0700

    Create function, option

---
+++ loop-option/loopme.sh
@@ -0,0 +1,67 @@
+#!/bin/bash 
+
+clear_screen(){
+  clear;
+}
+choose_language(){
+  delay=2
+  options="EN TH Time"
+  select opt in $options; do
+     if [ "$opt" = "EN" ]; then
+       voice="Samantha"
+       read_word;
+       break;
+     elif [ "$opt" = "TH" ]; then
+       voice="Kanya"
+       read_word;
+       break;
+     elif [ "$opt" = "Time" ]; then
+       voice="Kanya"
+       delay=30
+       tellmetime=true;
+       break;
+     else
+      echo "What's wrong with you?"
+     fi
+  done
+}
+loopme(){
+  clear_screen;
+  choose_language;
+  clear_screen;
+  while true; 
+    do 
+      if [ ! -z ${tellmetime} ]
+      then
+        read_time;
+      fi
+
+      echo -n "$word |"; 
+      say -v ${voice} "$word"; 
+      
+      sleep ${delay}; 
+    done;
+}
+read_word(){
+  read -p "Your word ? : " word;
+}
+read_time(){
+       word="ขณะนี้เวลา..,"`date +%H`" นาฬิกา ,"`date +%M`" นาที"
+}
+main(){
+  clear_screen;
+  echo "Welcome to loop me bash.."
+  options="Start Quit"
+  select opt in $options; do
+     if [ "$opt" = "Quit" ]; then
+      echo done
+      exit
+     elif [ "$opt" = "Start" ]; then
+      loopme ;
+     else
+      clear
+      echo bad option, try again?
+     fi
+  done
+}
+main;
